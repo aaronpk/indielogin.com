28de598c4269bbe1102c3a2d90e387600e0e60c4
Aaron Parecki <aaron@parecki.com>
2018-06-03 10:41:31 -0700

    add help form to login page

--- views/auth/login-form.php
+++ views/auth/login-form.php
@@ -2,20 +2,38 @@
 
 <div class="container container-narrow">
 
-  <p>Log in to <a href="<?= $client_id ?>"><?= $client_id ?></a></p>
 
-  <form action="/auth" method="get">
-    <div class="form-group">
-      <input type="url" placeholder="example.com" name="me" class="form-control">
-    </div>
+  <div class="login-details">
+    <p>Log in to <a href="<?= $client_id ?>"><?= $client_id ?></a></p>
 
-    <input type="hidden" name="client_id" value="<?= $client_id ?>">
-    <input type="hidden" name="redirect_uri" value="<?= $redirect_uri ?>">
-    <input type="hidden" name="state" value="<?= $state ?>">
+    <form action="/auth" method="get">
+      <div class="form-group">
+        <input type="url" placeholder="example.com" name="me" class="form-control">
+      </div>
 
-    <button class="btn btn-outline-secondary">Sign In</button>
-  </form>
+      <input type="hidden" name="client_id" value="<?= $client_id ?>">
+      <input type="hidden" name="redirect_uri" value="<?= $redirect_uri ?>">
+      <input type="hidden" name="state" value="<?= $state ?>">
 
-  <!-- TODO: add short docs like on https://indieauth.com/auth?redirect_uri=foo -->
+      <button class="btn btn-primary">Sign In</button>
+    </form>
+  </div>
+
+  <div class="setup-help">
+    <h3>Sign in with your Website</h3>
+
+    <div class="profile-examples"><img src="/images/profiles.png" style="width: 100%;"></div>
+
+    <p>This is a <a href="https://indieweb.org/web_sign-in">web sign-in</a> prompt. To use it, you'll need to:</p>
+
+    <ul>
+      <li>Add links on your website to your various social profiles (Twitter, Github, etc) with the attribute rel="me"</li>
+      <li>Ensure these profiles link back to your website</li>
+      <li>Alternatively, you can use your <a href="/setup#email">email</a> or <a href="/setup#pgp">PGP key</a></li>
+      <li>If your website supports the <a href="https://www.w3.org/TR/indieauth/">IndieAuth protocol</a> then this will work without any changes</li>
+    </ul>
+
+    <p>Read the <a href="/setup">full setup instructions</a></p>
+  </div>
 
 </div>
