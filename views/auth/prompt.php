72db738c3318f339b9c7e06f01a8f52a33d4d2a6
Aaron Parecki <aaron@parecki.com>
2018-06-03 10:29:12 -0700

    better formatting for logged-in screen

--- views/auth/prompt.php
+++ views/auth/prompt.php
@@ -2,19 +2,21 @@
 
 <div class="container container-narrow">
 
-  <p>Log in as <a href="<?= $me ?>"><?= $me ?></a></p>
-
-  <form action="/select" method="post">
-    <input type="hidden" name="code" value="<?= $code ?>">
-    <input type="submit" value="Continue" class="btn btn-primary">
-  </form>
-
   <div class="login-details">
+    <p>Log in as <a href="<?= $me ?>"><?= $me ?></a></p>
+
+    <form action="/select" method="post">
+      <input type="hidden" name="code" value="<?= $code ?>">
+      <input type="submit" value="Continue" class="btn btn-primary">
+    </form>
+  </div>
+
+  <div>
     <p>Logging in to <a href="<?= $client_id ?>"><?= $client_id ?></a></p>
 
     <p class="redirect_uri">You will be redirected to <?= $redirect_uri ?></p>
-  </div>
 
-  <p style="margin-top: 1em;"><a href="<?= $switch_account ?>">Log in as a different user</a></p>
+    <p style="margin-top: 1em;"><a href="<?= $switch_account ?>">Log in as a different user</a></p>
+  </div>
 
 </div>
