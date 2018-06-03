0a72d5e1c4193e55762f5aa2ba3ca5d707f870d8
Aaron Parecki <aaron@parecki.com>
2018-06-03 12:05:36 -0700

    add schema

---
+++ schema/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `clients` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` varchar(512) DEFAULT NULL,
+  `user_url` varchar(512) DEFAULT NULL,
+  `email` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `redirect_uris` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) DEFAULT NULL,
+  `redirect_uri` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `logins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `client_id` varchar(512) DEFAULT NULL,
+  `redirect_uri` varchar(512) DEFAULT NULL,
+  `authn_provider` varchar(20) DEFAULT NULL,
+  `authn_profile` varchar(512) DEFAULT NULL,
+  `me_entered` varchar(512) DEFAULT NULL,
+  `me_resolved` varchar(512) DEFAULT NULL,
+  `complete` tinyint(4) NOT NULL DEFAULT '0',
+  `date_complete` datetime DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
