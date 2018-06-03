9b9f907f5d99957a11d362defd7864e707e63229
Aaron Parecki <aaron@parecki.com>
2018-06-02 20:13:59 -0700

    add margin to alert

--- views/layout.php
+++ views/layout.php
@@ -43,7 +43,7 @@
     </div>
   </nav>
 
-  <div class="alert alert-info">This site is currently in Beta. Please report any errors <a href="https://github.com/aaronpk/IndieLogin.com">on GitHub</a>.</div>
+  <div class="alert alert-info" style="margin: 1em;">This site is currently in Beta. Please report any errors <a href="https://github.com/aaronpk/IndieLogin.com">on GitHub</a>.</div>
 
 
   <?= $this->section('content')?>
