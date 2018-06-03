b814d5bffd0ed82dd80534e8345fa78d451fea91
Aaron Parecki <aaron@parecki.com>
2018-06-03 09:16:11 -0700

    improved url auto prefix

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,10 +1,20 @@
 
-$(function(){
-
-  $("input[type=url]").on("blur", function(){
-    if(!$(this).val().match(/^https?:/)) {
-      $(this).val("http://"+$(this).val());
+  /* add http:// to URL fields on blur or when enter is pressed */
+  document.addEventListener('DOMContentLoaded', function() {
+    function addDefaultScheme(target) {
+      if(target.value.match(/^(?!https?:).+\..+/)) {
+        target.value = "http://"+target.value;
+      }
     }
+    var elements = document.querySelectorAll("input[type=url]");
+    Array.prototype.forEach.call(elements, function(el, i){
+      el.addEventListener("blur", function(e){
+        addDefaultScheme(e.target);
+      });
+      el.addEventListener("keydown", function(e){
+        if(e.keyCode == 13) {
+          addDefaultScheme(e.target);
+        }
+      });
+    });
   });
-
-});
